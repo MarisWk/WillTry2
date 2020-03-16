ffe66c547b94e5354260043edaab97b736cd4f09
Your Name <you@example.com>
2020-03-16 20:08:45 +0200

    Add Book button works

--- src/sample/Controllers/AddBookWindow.java
+++ src/sample/Controllers/AddBookWindow.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class AddBookWindow {
 
@@ -12,6 +13,15 @@ public class AddBookWindow {
 
     @FXML
     private URL location;
+
+    @FXML
+    private TextField addBookName;
+
+    @FXML
+    private TextField addBookAuthor;
+
+    @FXML
+    private TextField addBookYear;
 
     @FXML
     private Button AddBookWindowAddButton;
--- src/sample/Controllers/MainWindow.java
+++ src/sample/Controllers/MainWindow.java
@@ -1,9 +1,14 @@
 package sample.Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainWindow {
 
@@ -19,7 +24,22 @@ public class MainWindow {
     @FXML
     void initialize() {
         mainAddBookButton.setOnAction(event -> {
-            System.out.println("Hello World!");
+            mainAddBookButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/FrontEnd/AddBookWindow.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
         });
 
     }
